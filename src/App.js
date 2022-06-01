87b236ce15967d14676b82e0bc5bff8e017f2d05
syrgakusupov <usupovsyrgak5@gmail.com>
2022-06-01 14:04:39 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react'
+import Photo from '../src/img/kyrgyzstan-landscape-lake.jpg'
+import Images from '../src/img/istockphoto-1294990080-170667a.jpg'
+import Surot from '../src/img/20495308.jpg'
+import Cars from '../src/img/HByeKGm.jpg'
+import Sea from '../src/img/1080p-Wallpapers-Full-HD-Download.jpg' 
+import Mosque from '../src/img/Ismlaic-Architecture12.jpg'
 
 function App() {
+
+  const [count, setCount] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='images'>
+      <img className='photo' onClick={ () => setCount(Photo)} src={Photo}/>
+      <img className='image' onClick={ () => setCount(Images)} src={Images}/>
+      <img className='surot' onClick={ () => setCount(Surot)} src={Surot}/>
+      <img className='cars' onClick={ () => setCount(Cars)} src={Cars}/>
+      <img className='sea' onClick={ () => setCount(Sea)} src={Sea}/>
+      <img className='mosque' onClick={ () => setCount(Mosque)} src={Mosque}/>
+      </div>
+
+      <img className='count' src={count}/>
+      
+
+      
+      
     </div>
   );
 }
